36d98e2913e8e9a0136479dbb7ceaa461c603c08
unknown <kaden.zipfel@hotmail.com>
2018-09-08 15:56:46 -0700

    Add mobile nav animation

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,16 +1,22 @@
 // Open nav dropdown when hamburger menu is clicked
 document.getElementById('nav-menu').onclick = function() {
   document.getElementById('nav-dropdown').style.display = "flex";
+  document.getElementById('nav-menu').style.display = "none";
+  document.getElementById('close-nav').style.display = "flex";
 }
 
 // Close nav dropdown when x is clicked
 document.getElementById('close-nav').onclick = function() {
   document.getElementById('nav-dropdown').style.display = "none";
+  document.getElementById('close-nav').style.display = "none";
+  document.getElementById('nav-menu').style.display = "flex";
 }
 
 // Close nav dropdown when anything is clicked
 document.getElementById('nav-dropdown').onclick = function() {
   document.getElementById('nav-dropdown').style.display = "none";
+  document.getElementById('close-nav').style.display = "none";
+  document.getElementById('nav-menu').style.display = "flex";
 }
 
 // Shrink navbar when user scrolls
