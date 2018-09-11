0c31e5b30f60336a0c7b94c54e47992f5872aaf4
unknown <kaden.zipfel@hotmail.com>
2018-09-11 12:25:00 -0700

    Finalize navbar... hopefully

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -12,19 +12,26 @@ document.getElementById('close-nav').onclick = function() {
   document.getElementById('nav-menu').style.display = "flex";
 }
 
-// Close nav dropdown when anything is clicked
-document.getElementById('nav-dropdown').onclick = function() {
-  document.getElementById('nav-dropdown').style.display = "none";
-  document.getElementById('close-nav').style.display = "none";
-  document.getElementById('nav-menu').style.display = "flex";
+// Open gallery droplist when gallery is clicked
+document.getElementById('drop').onclick = function() {
+  document.getElementById('drop-list').style.display = "flex";
+  document.getElementById('gallery').style.display = "none";
 }
 
 // Shrink navbar when user scrolls
 window.onscroll = function() {
   var nav = document.getElementById('nav');
+  var gal = document.getElementById('gallery-dropdown');
+  var drop = document.getElementById('gallery-dropdown-list');
   if ( window.pageYOffset > 100 ) {
     nav.style.height = "7rem";
+    gal.onmouseover = function() {
+      drop.style.animation = "0.3s dropdown-scroll forwards";
+    }
   } else {
     nav.style.height = "12rem";
+    gal.onmouseover = function() {
+      drop.style.animation = "0.3s dropdown forwards";
+    }
   }
 }
