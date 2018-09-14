f08b0c57a13023cb2a5e3b5cf0e837904189a336
unknown <kaden.zipfel@hotmail.com>
2018-09-14 14:10:29 -0700

    Add booking popup

---
+++ public/js/booking.js
@@ -0,0 +1,53 @@
+// Open first popup on button click
+document.getElementById('booking-btn-1').onclick = function() {
+  document.getElementById('booking-popup-1').style.visibility = "visible";
+};
+// Close first popup on x click
+document.getElementById('booking-x-1').onclick = function() {
+  document.getElementById('booking-popup-1').style.visibility = "hidden";
+};
+
+// Open second popup on button click
+document.getElementById('booking-btn-2').onclick = function() {
+  document.getElementById('booking-popup-2').style.visibility = "visible";
+};
+// Close second popup on x click
+document.getElementById('booking-x-2').onclick = function() {
+  document.getElementById('booking-popup-2').style.visibility = "hidden";
+};
+
+// Open third popup on button click
+document.getElementById('booking-btn-3').onclick = function() {
+  document.getElementById('booking-popup-3').style.visibility = "visible";
+};
+// Close third popup on x click
+document.getElementById('booking-x-3').onclick = function() {
+  document.getElementById('booking-popup-3').style.visibility = "hidden";
+};
+
+// Open fourth popup on button click
+document.getElementById('booking-btn-4').onclick = function() {
+  document.getElementById('booking-popup-4').style.visibility = "visible";
+};
+// Close fourth popup on x click
+document.getElementById('booking-x-4').onclick = function() {
+  document.getElementById('booking-popup-4').style.visibility = "hidden";
+};
+
+// Open fifth popup on button click
+document.getElementById('booking-btn-5').onclick = function() {
+  document.getElementById('booking-popup-5').style.visibility = "visible";
+};
+// Close fifth popup on x click
+document.getElementById('booking-x-5').onclick = function() {
+  document.getElementById('booking-popup-5').style.visibility = "hidden";
+};
+
+// Open sixth popup on button click
+document.getElementById('booking-btn-6').onclick = function() {
+  document.getElementById('booking-popup-6').style.visibility = "visible";
+};
+// Close sixth popup on x click
+document.getElementById('booking-x-6').onclick = function() {
+  document.getElementById('booking-popup-6').style.visibility = "hidden";
+};
